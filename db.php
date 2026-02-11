db8143e699f6107b00bf9fc2de9cbf34c9018611
mariSugam69 <patgarrajesh80@gmail.com>
2026-02-11 19:38:48 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "catering_connect";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "catering_connect";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
